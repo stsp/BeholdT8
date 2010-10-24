d1cefeecd08e49d77e75cf7d2cc934cd0dc029c0
Mauro Carvalho Chehab <mchehab@redhat.com>
2010-10-24 20:49:52 -0200

    add a script to automate building
    This script will automate building, assuming that the backport patches
    will apply fine.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

---
+++ build.sh
@@ -0,0 +1,28 @@
+run ()
+{
+	echo $@
+	$@
+	if [ "$?" != "0" ]; then
+		echo "*** ERROR. Aborting ***"
+		exit -1
+	fi
+}
+
+VER="v`uname -r|cut -d'-' -f 1`"
+
+echo "***********************************************************"
+echo "* This script will download the latest tarball and build it"
+echo "* Assuming that your kernel is compatible with the latest  "
+echo "* drivers. If not, you'll need to add some extra backports,"
+echo "* ./backports/<kernel> directory.                          "
+echo "***********************************************************"
+sleep 5
+run make -C linux/ download
+run make -C linux/ untar
+run make -C linux/ apply-patches VER=$VER
+run make
+
+echo "**********************************************************"
+echo "* Compilation finished. Use "make install" to install them"
+echo "**********************************************************"
+
