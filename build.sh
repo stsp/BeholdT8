ae115dd8757ba55316d0ad1dc4a34ccf82e74d5e
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-01-23 13:51:37 -0200

    Be sure to have the latest compat bits before building the tree
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- build.sh
+++ build.sh
@@ -8,13 +8,17 @@ run ()
 	fi
 }
 
-echo "***********************************************************"
-echo "* This script will download the latest tarball and build it"
-echo "* Assuming that your kernel is compatible with the latest  "
-echo "* drivers. If not, you'll need to add some extra backports,"
-echo "* ./backports/<kernel> directory.                          "
-echo "***********************************************************"
+echo "************************************************************"
+echo "* This script will download the latest tarball and build it*"
+echo "* Assuming that your kernel is compatible with the latest  *"
+echo "* drivers. If not, you'll need to add some extra backports,*"
+echo "* ./backports/<kernel> directory.                          *"
+echo "* It will also update this tree to be sure that all compat *"
+echo "* bits are there, to avoid compilation failures            *"
+echo "************************************************************"
 sleep 5
+run git pull ssh://linuxtv.org/git/media_build master
+
 run make -C linux/ download
 run make -C linux/ untar
 run make
