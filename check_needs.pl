9f86bbad7efe41a2467677765e7bb304fffb5057
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-06-19 09:09:57 -0300

    move all checks to check_needs.pl
    This helps to add an OS-dependent logic on it
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- check_needs.pl
+++ check_needs.pl
@@ -21,11 +21,30 @@ sub need_program
 	$need++;
 }
 
+sub need_perl_module
+{
+	my $prog = shift;
+	my $pkgname = shift;
+
+	my $err = system("perl -M$prog -e 1 2>/dev/null /dev/null");
+	return if ($err == 0);
+	print "ERROR: please install \"$prog\", otherwise, build won't work.";
+	print " This program is generally found at \"$pkgname\" package." if ($pkgname);
+	print "\n";
+
+	$need++;
+}
+
+# Check for needed programs/tools
 need_program "git";
 need_program "make";
 need_program "gcc";
 need_program "patch";
 need_program "lsdiff", "patchutils";
+
+# Check for needed perl modules
+need_perl_module "Digest::SHA1", "perl-Digest-SHA1";
+need_perl_module "Proc::ProcessTable", "perl-Proc-ProcessTable";
 
 die "Build can't procceed as $need dependency is missing" if ($need == 1);
 die "Build can't procceed as $need dependencies are missing" if ($need);
