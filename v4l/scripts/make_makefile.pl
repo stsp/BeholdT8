efe9ea2ab4b86c54e3eb02e306cc09a2622fdc51
Igor M. Liplianin <liplianin@me.by>
2011-10-11 17:51:51 -0300

    Fixes altera-stapl build
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- v4l/scripts/make_makefile.pl
+++ v4l/scripts/make_makefile.pl
@@ -205,6 +205,7 @@ open OUT, '>Makefile.media' or die 'Unable to write Makefile.media';
 open_makefile('../linux/drivers/media/Makefile');
 
 find({wanted => \&parse_dir, no_chdir => 1}, '../linux/drivers/staging');
+find({wanted => \&parse_dir, no_chdir => 1}, '../linux/drivers/misc');
 
 # Creating Install rule
 print OUT "media-install::\n";
