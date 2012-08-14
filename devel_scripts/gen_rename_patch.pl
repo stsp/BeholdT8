b9551e9f61574541c45751e3946bf143a3bf50ac
Mauro Carvalho Chehab <mchehab@redhat.com>
2012-08-14 08:29:31 -0300

    devel_scripts: add two scripts to help dealing with file renames
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

---
+++ devel_scripts/gen_rename_patch.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use strict;
+
+my $debug=0;
+my $from;
+my $to;
+my $sed;
+
+while (<>) {
+	$from = $1 if (/^rename from (.*)/);
+	$to = $1 if (/^rename to (.*)/);
+
+	if ($from && $to) {
+		printf "$from -> $to\n" if ($debug);
+		$sed .= "\tsed s,'$from','$to',g | \\\n";
+		$from = 0;
+		$to = 0;
+	}
+}
+
+$sed =~ s/\s*\|\s* \\\s*$//;
+
+open OUT, ">rename_patch.sh";
+print OUT "#!/bin/bash\n\n";
+print OUT "if [ \"\$1\" == \"\" ]; then echo \"usage: $0 <in_patch>\"; exit -1; fi\n\n";
+print OUT "OLD=\"\$1.old\"\n";
+print OUT "mv \$1 \$OLD && ";
+print OUT "cat \$OLD | \\\n$sed >\$1 && \\\n";
+print OUT "echo -e \"old patch stored as \"\$OLD\"\\n\$1 rewrote.\"\n";
+close OUT;
+
+chmod(0755, "rename_patch.sh");
