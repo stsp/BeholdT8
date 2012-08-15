dccf830dafe13fee2cffc2018d679fa55b18971c
Mauro Carvalho Chehab <mchehab@redhat.com>
2012-08-15 17:57:05 -0300

    Improve the developer scripts
    Instead of creating lots of forks, just use one sed instance.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- devel_scripts/gen_rename_patch.pl
+++ devel_scripts/gen_rename_patch.pl
@@ -12,20 +12,21 @@ while (<>) {
 
 	if ($from && $to) {
 		printf "$from -> $to\n" if ($debug);
-		$sed .= "\tsed s,'$from','$to',g | \\\n";
+		$sed .= "\t-e 's,$from,$to,g' \\\n";
 		$from = 0;
 		$to = 0;
 	}
 }
 
 $sed =~ s/\s*\|\s* \\\s*$//;
+$sed =~ s/^\s*//;
 
 open OUT, ">rename_patch.sh";
 print OUT "#!/bin/bash\n\n";
 print OUT "if [ \"\$1\" == \"\" ]; then echo \"usage: $0 <in_patch>\"; exit -1; fi\n\n";
 print OUT "OLD=\"\$1.old\"\n";
 print OUT "mv \$1 \$OLD && ";
-print OUT "cat \$OLD | \\\n$sed >\$1 && \\\n";
+print OUT "cat \$OLD | sed $sed >\$1 && \\\n";
 print OUT "echo -e \"old patch stored as \"\$OLD\"\\n\$1 rewrote.\"\n";
 close OUT;
 
