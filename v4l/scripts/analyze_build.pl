49f8a925754136c170552b57173ad4c221976f60
Mauro Carvalho Chehab <mchehab@redhat.com>
2012-09-15 15:56:18 -0300

    analyze_build.pl: Use tab for alignment
    No functional changes here. Just use the standard 8-spaces tabs alignment
    for the script, just like the other files under this tree.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- v4l/scripts/analyze_build.pl
+++ v4l/scripts/analyze_build.pl
@@ -48,99 +48,99 @@ my %config = ();
 my %multi = ();
 
 sub open_makefile($) {
-    my $file = shift;
+	my $file = shift;
 
-    # only open a given Makefile once
-    return if exists $makefiles{$file};
-    $makefiles{$file} = 1;
+	# only open a given Makefile once
+	return if exists $makefiles{$file};
+	$makefiles{$file} = 1;
 
-    $file =~ m|^(.*)/[^/]*$|;
-    my $dir = $1;
+	$file =~ m|^(.*)/[^/]*$|;
+	my $dir = $1;
 
-#    print STDERR "opening $root$file (dir=$dir)\n";
-    my $in = new FileHandle;
-    open $in, '<', "$root$file" or die "Unable to open Makefile '$root$file': $!";
+	#    print STDERR "opening $root$file (dir=$dir)\n";
+	my $in = new FileHandle;
+	open $in, '<', "$root$file" or die "Unable to open Makefile '$root$file': $!";
 
-    while (<$in>) {
-	# print STDERR "Line: $_";
-	# Skip comment and blank lines
-	next if (/^\s*(#.*)?$/);
-	m/^\s*\-?include/ and die "Can't handle includes! In $file";
+	while (<$in>) {
+		# print STDERR "Line: $_";
+		# Skip comment and blank lines
+		next if (/^\s*(#.*)?$/);
+		m/^\s*\-?include/ and die "Can't handle includes! In $file";
 
-	# Handle line continuations
-	if (/\\\n$/) {
-	    $_ .= <$in>;
-	    redo;
-	}
-	# Eat line continuations in string we will parse
-	s/\s*\\\n\s*/ /g;
-	# Eat comments
-	s/#.*$//;
-
-	if (/^\s*obj-(\S+)\s*([:+]?)=\s*(\S.*?)\s*$/) {
-	    print STDERR "Should use '+=' in $file:$.\n$_\n" if ($check && $2 ne '+');
-	    my ($var,$targets) = ($1, $3);
-	    if ($var =~ /\$\(CONFIG_(\S+)\)$/) {
-		$var = $1;
-	    } elsif ($var !~ /^[ym]$/) {
-		print STDERR "Confused by obj assignment '$var' in $file:$.\n$_";
-	    }
-	    foreach(split(/\s+/, $targets)) {
-		if (m|/$|) { # Ends in /, means it's a directory
-		    open_makefile("$dir/$_".'Makefile');
-		} elsif (/^(\S+)\.o$/) {
-		    push @{$config{$var}}, "$dir/$1";
-		} else {
-		    print STDERR "Confused by target '$_' in $file:$.\n";
+		# Handle line continuations
+		if (/\\\n$/) {
+			$_ .= <$in>;
+			redo;
 		}
-	    }
-	    next;
+		# Eat line continuations in string we will parse
+		s/\s*\\\n\s*/ /g;
+		# Eat comments
+		s/#.*$//;
+
+		if (/^\s*obj-(\S+)\s*([:+]?)=\s*(\S.*?)\s*$/) {
+			print STDERR "Should use '+=' in $file:$.\n$_\n" if ($check && $2 ne '+');
+			my ($var,$targets) = ($1, $3);
+			if ($var =~ /\$\(CONFIG_(\S+)\)$/) {
+				$var = $1;
+			} elsif ($var !~ /^[ym]$/) {
+				print STDERR "Confused by obj assignment '$var' in $file:$.\n$_";
+			}
+			foreach(split(/\s+/, $targets)) {
+				if (m|/$|) { # Ends in /, means it's a directory
+					open_makefile("$dir/$_".'Makefile');
+				} elsif (/^(\S+)\.o$/) {
+					push @{$config{$var}}, "$dir/$1";
+				} else {
+					print STDERR "Confused by target '$_' in $file:$.\n";
+				}
+			}
+			next;
+		}
+		if (/(\S+)-objs\s*([:+]?)=\s*(\S.*?)\s*$/) {
+			my @files = split(/\s+/, $3);
+			foreach my $f (@files) {
+				$f =~ s|^(.*)\.o$|$dir/$1|;
+			}
+			if ($2 eq '+') {
+				# Adding to files
+				print STDERR "Should use ':=' in $file:$.\n$_\n" if ($check && !exists $multi{"$dir/$1"});
+				push @files, split(/\s+/, $multi{"$dir/$1"});
+			} else {
+				print STDERR "Setting objects twice in $file:$.\n$_\n" if ($check && exists $multi{"$dir/$1"});
+			}
+			$multi{"$dir/$1"} = "@files";
+			next;
+		}
+		if (/^\s*EXTRA_CFLAGS\s*([:+]?)=\s*(\S.*?)\s*$/) {
+			if ($check) {
+				sub allI { /^-I/ or return 0 foreach split(/\s+/, $_[0]);return 1; }
+				my $use = allI($2) ? ':' : '+';
+			print STDERR "Should use '$use=' with EXTRA_CFLAGS in $file:$.\n$_\n"
+			if ($1 ne $use);
+			}
+			next;
+		}
+		print STDERR "Odd line $file:$.\n$_\n" if ($check);
 	}
-	if (/(\S+)-objs\s*([:+]?)=\s*(\S.*?)\s*$/) {
-	    my @files = split(/\s+/, $3);
-	    foreach my $f (@files) {
-		$f =~ s|^(.*)\.o$|$dir/$1|;
-	    }
-	    if ($2 eq '+') {
-		# Adding to files
-		print STDERR "Should use ':=' in $file:$.\n$_\n" if ($check && !exists $multi{"$dir/$1"});
-		push @files, split(/\s+/, $multi{"$dir/$1"});
-	    } else {
-		print STDERR "Setting objects twice in $file:$.\n$_\n" if ($check && exists $multi{"$dir/$1"});
-	    }
-	    $multi{"$dir/$1"} = "@files";
-	    next;
-	}
-	if (/^\s*EXTRA_CFLAGS\s*([:+]?)=\s*(\S.*?)\s*$/) {
-	    if ($check) {
-		sub allI { /^-I/ or return 0 foreach split(/\s+/, $_[0]);return 1; }
-		my $use = allI($2) ? ':' : '+';
-		print STDERR "Should use '$use=' with EXTRA_CFLAGS in $file:$.\n$_\n"
-		    if ($1 ne $use);
-	    }
-	    next;
-	}
-	print STDERR "Odd line $file:$.\n$_\n" if ($check);
-    }
 }
 
 if ($#ARGV >= 0) {
-    $prefix = $ARGV[0];
-    $prefix .= '/' unless ($prefix =~ m|/$|);
+	$prefix = $ARGV[0];
+	$prefix .= '/' unless ($prefix =~ m|/$|);
 }
 
 # Find root of source tree: command line, then Hg command, lastly cwd
 if ($#ARGV >= 1) {
-    $root = $ARGV[1];
-    $root .= '/' unless ($root =~ m|/$|);
+	$root = $ARGV[1];
+	$root .= '/' unless ($root =~ m|/$|);
 } else {
-    $root = `hg root 2>/dev/null`;
-    if($? == 0) {
-	chomp $root;
-	$root .= '/';
-    } else {
-	$root = '';
-    }
+	$root = `hg root 2>/dev/null`;
+	if($? == 0) {
+		chomp $root;
+		$root .= '/';
+	} else {
+		$root = '';
+	}
 }
 print "# Using source tree root '$root'\n" if ($root ne '');
 
@@ -148,40 +148,38 @@ open_makefile($prefix."Makefile");
 
 print "# Kconfig variable = kernel modules built\n";
 foreach my $var (keys %config) {
-    my @list = @{$config{$var}};
-    map { s/^$prefix(.*)$/\1.ko/ } @list;
-    printf "%-22s= %s\n", $var, join(' ', @list);
+	my @list = @{$config{$var}};
+	map { s/^$prefix(.*)$/\1.ko/ } @list;
+	printf "%-22s= %s\n", $var, join(' ', @list);
 }
 
 print "\n# kernel module = source files\n";
 my %modules = ();
 foreach my $mods (values %config) {
-    $modules{$_} = 1 foreach @$mods;
+	$modules{$_} = 1 foreach @$mods;
 }
 foreach (keys %modules) {
-    /$prefix(.*)$/;
-    printf "%-30s = ", "$1.ko";
-    if (exists $multi{$_}) {
-	my @list = split(/\s+/, $multi{$_});
-	map { s/^$prefix(.*)$/\1.c/ } @list;
-	print join(' ', @list), "\n";
-    } else {
-	print "$1.c\n";
-    }
+	/$prefix(.*)$/;
+	printf "%-30s = ", "$1.ko";
+	if (exists $multi{$_}) {
+		my @list = split(/\s+/, $multi{$_});
+		map { s/^$prefix(.*)$/\1.c/ } @list;
+		print join(' ', @list), "\n";
+	} else {
+		print "$1.c\n";
+	}
 }
 
 print "\n# Kconfig varible = source files\n";
 while (my ($var, $list) = each %config) {
-    my @outlist = ();
-    foreach (@$list) {
-	if (exists $multi{$_}) {
-	    push @outlist, split(/\s+/, $multi{$_});
-	} else {
-	    push @outlist, $_;
+	my @outlist = ();
+	foreach (@$list) {
+		if (exists $multi{$_}) {
+			push @outlist, split(/\s+/, $multi{$_});
+		} else {
+			push @outlist, $_;
+		}
 	}
-    }
-    map { s/^$prefix(.*)$/\1.c/ } @outlist;
-    printf "%-22s= %s\n", $var, join(' ', @outlist);
+	map { s/^$prefix(.*)$/\1.c/ } @outlist;
+	printf "%-22s= %s\n", $var, join(' ', @outlist);
 }
-
-exit;
