5196131dcdc98e5958e25f74821d2dc8e9c50c6f
Hans Verkuil <hans.verkuil@cisco.com>
2013-05-22 13:29:06 +0200

    Fix compatibility issues since latest merge.
    - Add SIMPLE_DEV_PM_OPS support
    - Add vm_iomap_memory support
    
    Only build adv7183, vs6624 and NOON010PC30 for kernels >= 3.5
    since they need devm_gpio_request_one.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

--- v4l/compat.h
+++ v4l/compat.h
@@ -1175,4 +1175,52 @@ static inline void proc_set_size(struct proc_dir_entry *de, loff_t size) {}
 #endif
 #endif
 
+#ifdef NEED_SIMPLE_DEV_PM_OPS
+#define SIMPLE_DEV_PM_OPS(name, suspend_fn, resume_fn) \
+	struct dev_pm_ops name = { \
+		        .suspend = suspend_fn, \
+		        .resume = resume_fn, \
+		        .freeze = suspend_fn, \
+		        .thaw = resume_fn, \
+		        .poweroff = suspend_fn, \
+		        .restore = resume_fn, \
+	}
+#endif
+
+#ifdef NEED_VM_IOMAP_MEMORY
+#include <linux/mm.h>
+static inline int vm_iomap_memory(struct vm_area_struct *vma, phys_addr_t start, unsigned long len)
+{
+	unsigned long vm_len, pfn, pages;
+
+	/* Check that the physical memory area passed in looks valid */
+	if (start + len < start)
+		return -EINVAL;
+	/*
+	 * You *really* shouldn't map things that aren't page-aligned,
+	 * but we've historically allowed it because IO memory might
+	 * just have smaller alignment.
+	 */
+	len += start & ~PAGE_MASK;
+	pfn = start >> PAGE_SHIFT;
+	pages = (len + ~PAGE_MASK) >> PAGE_SHIFT;
+	if (pfn + pages < pfn)
+		return -EINVAL;
+
+	/* We start the mapping 'vm_pgoff' pages into the area */
+	if (vma->vm_pgoff > pages)
+		return -EINVAL;
+	pfn += vma->vm_pgoff;
+	pages -= vma->vm_pgoff;
+
+	/* Can we fit all of the mapping? */
+	vm_len = vma->vm_end - vma->vm_start;
+	if (vm_len >> PAGE_SHIFT > pages)
+		return -EINVAL;
+
+	/* Ok, let it rip */
+	return io_remap_pfn_range(vma, vma->vm_start, pfn, vm_len, vma->vm_page_prot);
+}
+#endif
+
 #endif /*  _COMPAT_H */
