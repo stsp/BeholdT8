1f768a7d5c94c76df7702fad05529f4168269ada
Hans Verkuil <hans.verkuil@cisco.com>
2015-09-22 14:21:14 +0200

    Install frame_vector.ko if needed.
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

--- v4l/Makefile.mm
+++ v4l/Makefile.mm
@@ -7,7 +7,8 @@ KDIRA          := /lib/modules/$(KERNELRELEASE)/kernel
 mm-install install-mm::
 	@dir="mm"; \
 	files='frame_vector.ko'; \
+	if [ -f $$files ]; then \
 	echo -e "\nInstalling $(KDIRA)/$$dir files:"; \
 	install -d $(KDIRA)/$$dir; \
 	for i in $$files;do if [ -e $$i ]; then echo -n "$$i "; \
-	install -m 644 -c $$i $(KDIRA)/$$dir; fi; done; echo;
+	install -m 644 -c $$i $(KDIRA)/$$dir; fi; done; echo; fi
